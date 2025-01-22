1f9a4f316cddeae6214990af53dc730b2bd5e4e8
Lavken <lavkenpilar@gmail.com>
2025-01-22 13:21:53 -0300

    se modifica el input por un select con 10 opciones

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,45 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Personajes from "./Personajes";
+
 
 export default function Home () {
-    const [id, setId] = useState("")
+    //const [id, setId] = useState ("")
+    const [seleccion, setSeleccion] = useState("")
     const navigate = useNavigate()
-
     
-    const irAPersonajes=()=>{
-    navigate(`/personajes/${id}`) 
+    const opciones = [
+        {id:1, nombre:"Opción1"},
+        {id:2, nombre:"Opción2"},
+        {id:3, nombre:"Opción3"},
+        {id:4, nombre:"Opción4"},
+        {id:5, nombre:"Opción5"},
+        {id:6, nombre:"Opción6"},
+        {id:7, nombre:"Opción7"},
+        {id:8, nombre:"Opción8"},
+        {id:9, nombre:"Opción9"},
+        {id:10, nombre:"Opción10"},
+    ]
+    const manejarSeleccion = (event) => {
+        setSeleccion (event.target.value)
     }
+    const irAPersonajes=()=>{
+    navigate(`/personajes/${seleccion}`) 
+    }
+
 return (
     <div className="mt-5">
-        <h1>React Router II</h1>
-        <input
+        <h1>React Router II</h1> 
+        <select
         type="number"
-        value={id}
-        onChange= {({target}) => setId(target.value)}>            
-        </input>      
+        value={seleccion}
+        onChange= {manejarSeleccion}>    
+        <option value=""> Selecciona...</option> {/*opcion por defecto*/}
+        {opciones.map ( (opcion)=>(
+            <option key={opcion.id} value={opcion.id}>
+                {opcion.nombre}
+            </option>
+       ))}       
+        </select>      
         <button onDoubleClick={ irAPersonajes }>Buscar</button>
     </div>
 ) }
